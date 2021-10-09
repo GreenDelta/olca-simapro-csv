c6c779b06ef1abf40de1bb8c4331baf9e08addff
msrocka <michael.srocka@gmail.com>
2021-10-09 10:03:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# olca-simapro-csv
+
+https://github.com/laurepatou/IMPACT-World-in-Simapro
+https://github.com/massimopizzol/Simapro-CSV-converter
+
+* `;;;;;`
---
+++ src/main/java/org/openlca/simapro/csv/SimaProCsv.java
@@ -0,0 +1,37 @@
+package org.openlca.simapro.csv;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public final class SimaProCsv {
+
+  private SimaProCsv() {
+  }
+
+  /**
+   * The default character set of SimaPro CSV is Windows 1252.
+   *
+   * @return the Windows 1252 character set
+   */
+  public static Charset defaultCharset() {
+    return Charset.forName("windows-1252");
+  }
+
+  public static Reader readerOf(File file) {
+    return readerOf(file, defaultCharset());
+  }
+
+  public static Reader readerOf(File file, Charset charset) {
+    if (file == null)
+      return null;
+
+    return null;
+  }
+
+  public static Reader readerOf(InputStream stream, Charset charset) {
+    return null;
+  }
+
+}
