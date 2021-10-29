a8e83a78ba61f53f106eb92f2314ab464b0fbd34
Miguel Garcia Casas <miguel.g.c.505@gmail.com>
2021-10-29 18:17:19 +0200

    Add ElementaryFlowBlockTest and ParameterBlockTest

--- src/test/java/org/openlca/simapro/csv/ElementaryFlowBlockTest.java
+++ src/test/java/org/openlca/simapro/csv/ElementaryFlowBlockTest.java
@@ -13,7 +13,39 @@ public class ElementaryFlowBlockTest {
     for (var type : ElementaryFlowType.values()) {
       var flows = ds.getElementaryFlows(type);
       assertEquals(1, flows.size());
-      // TODO: @mg test flow values
+      var flow = flows.get(0);
+      if (flow.name().equals("Acids")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("", flow.cas());
+      }
+      if (flow.name().equals("(+-)-Citronellol")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("026489-01-0", flow.cas());
+      }
+      if (flow.name().equals("(1r,4r)-(+)-Camphor")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("000464-49-3", flow.cas());
+      }
+      if (flow.name().equals("1'-Acetoxysafrole")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("034627-78-6", flow.cas());
+      }
+      if (flow.name().equals("Asbestos")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("", flow.cas());
+      }
+      if (flow.name().equals("Noise from bus km")) {
+        assertEquals("km", flow.unit());
+        assertEquals("", flow.cas());
+      }
+      if (flow.name().equals("venting of argon, crude, liquid")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("", flow.cas());
+      }
+      if (flow.name().equals("Sample economic issue")) {
+        assertEquals("kg", flow.unit());
+        assertEquals("", flow.cas());
+      }
     }
   }
 
